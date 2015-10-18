f7e9a80d338b9bf5ec5b0bb13e968b3269102677
bgr11n <bgr11n@gmail.com>
2015-10-18 16:41:44 +0300

    basic auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 Rails.application.routes.draw do
+  get 'log_out', to: 'sessions#destroy', as: 'log_out'
+  get 'log_in', to: 'sessions#new', as: 'log_in'
+  resources :sessions
+
+  get 'sign_up', to: 'users#new', as: 'sign_up'
+  root to: 'users#new'
+  resources :users
 end
